6e682a64ed3364251224fd7f9e84f3ec35747193
ʏᴀᴛᴏ <YatoSama064@gmail.com>
2025-04-21 13:39:11 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,6 +1,8 @@
 import motor.motor_asyncio
+import datetime
 from config import Config
 from .utils import send_log
+
 
 class Database:
 
@@ -13,17 +15,26 @@ class Database:
 
     def new_user(self, id):
         return dict(
-            _id=int(id),                                   
+            _id=int(id),
+            join_date=datetime.date.today().isoformat(),
             file_id=None,
             caption=None,
-            format_template=None  # Add this line for the format template
+            format_template=None,
+            metadata=True,
+            metadata_code="Telegram : @Codeflix_Bots",
+            ban_status=dict(
+                is_banned=False,
+                ban_duration=0,
+                banned_on=datetime.date.max.isoformat(),
+                ban_reason=''
+            )
         )
 
     async def add_user(self, b, m):
         u = m.from_user
         if not await self.is_user_exist(u.id):
             user = self.new_user(u.id)
-            await self.col.insert_one(user)            
+            await self.col.insert_one(user)
             await send_log(b, u)
 
     async def is_user_exist(self, id):
@@ -40,7 +51,7 @@ class Database:
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'_id': int(user_id)})
-    
+
     async def set_thumbnail(self, id, file_id):
         await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
 
@@ -58,27 +69,67 @@ class Database:
     async def set_format_template(self, id, format_template):
         await self.col.update_one({'_id': int(id)}, {'$set': {'format_template': format_template}})
 
-    #async def set_start_msg(self, msgs):
-        #await self.start_msg.update_one({},{'$set': {'text_msg': msgs})
-
-    #async def get_text_msg(self):
-        #document = await self.start_msg.find_one({}, {'text_msg': 1})
-        #return document.get('text_msg') if document else None
-
-
     async def get_format_template(self, id):
         user = await self.col.find_one({'_id': int(id)})
         return user.get('format_template', None)
-        
+
     async def set_media_preference(self, id, media_type):
         await self.col.update_one({'_id': int(id)}, {'$set': {'media_type': media_type}})
-        
+
     async def get_media_preference(self, id):
         user = await self.col.find_one({'_id': int(id)})
         return user.get('media_type', None)
 
+    async def get_metadata(self, user_id):
+        user = await self.col.find_one({'_id': int(user_id)})
+        return user.get('metadata', "Off")
 
-    # Methods for AUTHO_USER set using the separate collection
+    async def set_metadata(self, user_id, metadata):
+        await self.col.update_one({'_id': int(user_id)}, {'$set': {'metadata': metadata}})
+
+    async def get_title(self, user_id):
+        user = await self.col.find_one({'_id': int(user_id)})
+        return user.get('title', 'Encoded by @Animes_Cruise')
+
+    async def set_title(self, user_id, title):
+        await self.col.update_one({'_id': int(user_id)}, {'$set': {'title': title}})
+
+    async def get_author(self, user_id):
+        user = await self.col.find_one({'_id': int(user_id)})
+        return user.get('author', '@Animes_Cruise')
+
+    async def set_author(self, user_id, author):
+        await self.col.update_one({'_id': int(user_id)}, {'$set': {'author': author}})
+
+    async def get_artist(self, user_id):
+        user = await self.col.find_one({'_id': int(user_id)})
+        return user.get('artist', '@Animes_Cruise')
+
+    async def set_artist(self, user_id, artist):
+        await self.col.update_one({'_id': int(user_id)}, {'$set': {'artist': artist}})
+
+    async def get_audio(self, user_id):
+        user = await self.col.find_one({'_id': int(user_id)})
+        return user.get('audio', 'By @Animes_Cruise')
+
+    async def set_audio(self, user_id, audio):
+        await self.col.update_one({'_id': int(user_id)}, {'$set': {'audio': audio}})
+
+    async def get_subtitle(self, user_id):
+        user = await self.col.find_one({'_id': int(user_id)})
+        return user.get('subtitle', "By @Animes_Cruise")
+
+    async def set_subtitle(self, user_id, subtitle):
+        await self.col.update_one({'_id': int(user_id)}, {'$set': {'subtitle': subtitle}})
+
+    async def get_video(self, user_id):
+        user = await self.col.find_one({'_id': int(user_id)})
+        return user.get('video', 'Encoded By @Animes_Cruise')
+
+    async def set_video(self, user_id, video):
+        await self.col.update_one({'_id': int(user_id)}, {'$set': {'video': video}})
+
+    # Autho user methods
     async def add_autho_user(self, id):
         if not await self.is_autho_user_exist(id):
             await self.autho_user_col.insert_one({'_id': int(id)})
@@ -98,12 +149,4 @@ class Database:
         return users
 
 
-
 madflixbotz = Database(Config.DB_URL, Config.DB_NAME)
-        
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
