cda71d094c1963aec50787017cfc1017ff5840e0
rohit3607 <rohitplayer87089@gmail.com>
2025-04-21 20:19:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ from config import Config
 from aiohttp import web
 from route import web_server
 
+PORT = Config.PORT
+
 class Bot(Client):
 
     def __init__(self):
@@ -28,7 +30,7 @@ class Bot(Client):
         if Config.WEBHOOK:
             app = web.AppRunner(await web_server())
             await app.setup()       
-            await web.TCPSite(app, "0.0.0.0", 5374).start()     
+            await web.TCPSite(app, "0.0.0.0", PORT).start()     
         print(f"{me.first_name} Is Started.....✨️")
         for id in Config.ADMIN:
             try: await self.send_message(id, f"**{me.first_name}  Is Started.....✨️**")                                
